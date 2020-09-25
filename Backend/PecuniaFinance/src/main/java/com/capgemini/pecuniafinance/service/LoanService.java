71aa5f93bffa32d54509635e3d4f659a68fd6a86
indirab76 <48474814+indirab76@users.noreply.github.com>
2020-09-25 12:31:25 +0530

    Update LoanService.java

--- Backend/PecuniaFinance/src/main/java/com/capgemini/pecuniafinance/service/LoanService.java
+++ Backend/PecuniaFinance/src/main/java/com/capgemini/pecuniafinance/service/LoanService.java
@@ -1,5 +1,16 @@
 package com.capgemini.pecuniafinance.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.capgemini.pecuniafinance.model.Loan;
+
+@Component
 public interface LoanService {
 
+	public String loanDisbursal(Loan loan);
+	public List<Loan> getLoanHistory (long account_id);
+	public Loan addLoanRequest(Loan loan);
+		
 }
