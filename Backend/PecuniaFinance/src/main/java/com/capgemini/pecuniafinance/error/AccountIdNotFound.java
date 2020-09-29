030fe2c4a2ed94670c2355c4926e93ef764fcc18
Vishruti98 <34179332+Vishruti98@users.noreply.github.com>
2020-09-29 14:58:42 +0530

    Backend Update

--- Backend/PecuniaFinance/src/main/java/com/capgemini/pecuniafinance/error/AccountIdNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.pecuniafinance.error;
-
-@SuppressWarnings("serial")
-public class AccountIdNotFound extends Exception{
-	public AccountIdNotFound(String errorMsg){
-		super(errorMsg);
-	}
-
-}
